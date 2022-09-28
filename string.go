c6679c28f7d7585889ea0376e4cabb21c48d0941
daffa.zahrandika <daffa.zahrandika@usenobi.com>
2022-09-28 19:49:22 +0700

    first commit

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhammad")
+	fmt.Println("Muhammad Daffa")
+	fmt.Println("Muhammad Daffa ZW")
+
+	fmt.Println(len("Muhammad"))
+	fmt.Println("Muhammad Daffa"[0])
+	fmt.Println("Muhammad Daffa ZW"[1])
+}
